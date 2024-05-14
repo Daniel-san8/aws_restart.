9bbf865f7f4816b4aff9ccdb138c36ad30020928
Daniel San <123564270+Daniel-san8@users.noreply.github.com>
2024-05-14 20:54:45 -0300

    Add files via upload

---
+++ Categorização.py
@@ -0,0 +1,7 @@
+lista = [9595, 45, 290578, 1.02, True, "My dog is on the bed.", "45"]
+
+for item in lista:
+    print("{} is of the data type {}".format(item, type(item)))
+    
+    
+    
---
+++ Strings.py
@@ -0,0 +1,57 @@
+myString = "This is a string"
+
+print(myString)
+
+print(type(myString))
+
+print(myString + " is of the data type " + str(type(myString)))
+
+firstString = "water"
+
+secondString = "fall"
+
+thirdString = firstString + secondString
+
+print(thirdString)
+
+name = input("What is your name? ")
+
+print(name)
+
+color = input("What is your favorite color?  ")
+
+animal = input("What is your favorite animal?  ")
+
+print("{}, you like a {} {}!".format(name,color,animal))
+
+
+
+
+
+
+dicionarioDaniel = {
+    "DanielIdade" : 20,
+    "IsaIdade" : 22
+}
+
+print(dicionarioDaniel["IsaIdade"])
+
+
+novaLista = [58, 2, 5, 8, 1]
+
+
+
+
+for numero in range(len(novaLista)):
+    novaLista[numero] = 1000
+    
+    
+    
+print(novaLista)
+
+
+nomePessoa = " Gabrielaaaaaaaaaaaaa "
+
+print(len(nomePessoa))
+
+    
